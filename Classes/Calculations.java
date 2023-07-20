9ff19fc6bc318057cddca04e4873cfb3fde1e0ad
Abdullah Salih Öner <64440843+AbdullahSalihOner@users.noreply.github.com>
2023-07-21 01:31:14 +0300

    21/07/23.1

---
+++ Classes/Calculations.java
@@ -0,0 +1,16 @@
+package Classes;
+
+public class Calculations {
+    public int Sum(int a, int b) {
+        return a + b;
+    }
+    public int Difference(int a, int b) {
+        return a - b;
+    }   
+    public int Product(int a, int b) {
+        return a * b;
+    }   
+    public int Quotient(int a, int b) {
+        return a / b;
+    }
+}
---
+++ Classes/Main.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculations calc = new Calculations();
+        System.out.println("Sum: " + calc.Sum(1, 2));
+        System.out.println("Difference: " + calc.Difference(1, 2)); 
+        System.out.println("Product: " + calc.Product(1, 2));   
+        System.out.println("Quotient: " + calc.Quotient(1, 2)); 
+
+    }
+}
